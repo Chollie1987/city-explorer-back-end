a5996e433a4e72b2363392c83260b1be30fc1699
Christina Hollie <christinahollie7@gmail.com>
2023-05-30 16:56:51 -0700

    added movies to backend

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const weatherData = require('./data/weather.json');
+const axios = require('axios');
 
 // initilizing express
 const app = express();
@@ -20,24 +21,51 @@ app.get('/', (request, response) => {
 
 //http://localhost:3001/shoppingList
 //http://render-app-name/shoppingList
-app.get('/weather', (request, response, next) => {
-    const { searchQuery } = request.query;
+app.get('/weather', async (request, response, next) => {
+    const { lat,lon } = request.query;
 
     try {
-     const city = weatherData.find(c => searchQuery.toLowerCase() === c.city_name.toLowerCase())
-     if (!city) {response.status(500).send('invalid city')
-    return
+        let url = `http://api.weatherbit.io/v2.0/forecast/daily?&key=${process.env.WEATHERBIT_KEY}&lat=${lat}&lon=${lon}&days=5`
+        console.log(url);
+      let res = await axios.get(url)
+      let formattedData = res.data.data.map(day => new Forecast(day))
+      response.status(200).send(formattedData)
+    //  const city = weatherData.find(c => searchQuery.toLowerCase() === c.city_name.toLowerCase())
+    //  if (!city) {response.status(500).send('invalid city')
+    // return
     }
-     const cityForcast = city.data.map(f => new Forcast(f))
-     response.status(200).send(cityForcast)
-    }
+    //  const cityForcast = city.data.map(f => new Forcast(f))
+    //  response.status(200).send(cityForcast)
     catch (error) {
         next(error);
     }
 
 });
 
-class Forcast {
+app.get('/movies', async (request, response, next) => {
+    const city = request.query.city
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIEDB_KEY}&query=${city}`
+    const results = await axios.get(url)
+    console.log(results.data);
+    const movieArray = results.data.results.map(movie => {
+        return new Movie(movie) 
+    })
+    response.status(200).send(movieArray.splice(0, 4))
+});
+
+class Movie {
+    constructor(obj) {
+       this.title = obj.title
+       this.overview = obj.overview
+       this.average_votes = obj.vote_average
+       this.total_votes = obj.vote_count
+        this.image_url =`https://image.tmdb.org/t/p/w500${obj.poster_path}`
+       this.popularity = obj.popularity
+       this.released_on = obj.release_date
+    }
+}
+
+class Forecast {
     constructor(obj) {
         this.date = obj.datetime;
         this.description = obj.weather.description
